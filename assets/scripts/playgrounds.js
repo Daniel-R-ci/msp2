761c1b4ed14a050308092534dd50d6180a498197
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-04-02 17:44:20 +0200

    Implement Jest testing for information in playgrounds.js, and document it in ReadMe

--- assets/scripts/playgrounds.js
+++ assets/scripts/playgrounds.js
@@ -1,1 +1,30 @@
 /* File containing all information about playgrounds */
+
+/* Copy the following lines into the array playgrounds when adding new playground
+{
+  "name": "", //Required value
+  "area": "", //Required value
+  "movements": ["ground", "feet", "air"], //Required array, must contain at least one item
+  "description": "",
+  "geoX": "",
+  "geoY": "",
+  "adress": "", //Required value
+  "image": "",
+},
+  */
+
+const playgrounds = [
+  {
+    "name": "Test park 1", //Required value
+    "area": "Hisingen",
+    "movements": ["ground", "feet", "water"], //Required array, must contain at least one item
+    "description": "",
+    "geoX": "",
+    "geoY": "",
+    "adress": "", //Required value
+    "image": "",
+  },
+
+];
+
+module.exports = playgrounds;
--- assets/scripts/tests/playgrounds.test.js
+++ assets/scripts/tests/playgrounds.test.js
@@ -1,1 +1,57 @@
 /* File containing tests for validating data in playgrounds.js */
+
+const playgrounds = require("../playgrounds");
+
+describe("Playgrounds validation", () => {
+  describe("Playgrounds array exist and has values", () => {
+    test("Playgrounds array length is greater than 0 ", () => {
+      expect(playgrounds.length).toBeGreaterThan(0);
+    });
+    for (let playground of playgrounds) {
+      describe("Every object in playgrounds array should have eight keys, and the correct keys", () => {
+        test("Make sure playground eight keys", () => {
+          expect(Object.keys(playground).length).toEqual(8);
+        });
+        test("Current playground has name key", () => {
+          expect(playground.name).toBeDefined();
+        });
+        test("Current playground has name key", () => {
+          expect(playground.area).toBeDefined();
+        });
+        test("Current playground has movements key", () => {
+          expect(playground.movements).toBeDefined();
+        });
+        test("Current playground has description key", () => {
+          expect(playground.description).toBeDefined();
+        });
+        test("Current playground has geoX key", () => {
+          expect(playground.geoX).toBeDefined();
+        });
+        test("Current playground has geoY key", () => {
+          expect(playground.geoY).toBeDefined();
+        });
+        test("Current playground has geoY key", () => {
+          expect(playground.geoY).toBeDefined();
+        });
+        test("Current playground has adress key", () => {
+          expect(playground.adress).toBeDefined();
+        });
+      });
+      describe("Required fields should have values", () =>{
+        test("Name should have value", () =>{
+          expect(playground.name.length).toBeGreaterThan(0);
+        });
+        test("Area should have value", () =>{
+          expect(playground.area.length).toBeGreaterThan(0);
+        });
+        test("Movements should have values", () =>{
+          expect(playground.movements.length).toBeGreaterThan(0);
+        });
+        test("Movements should not have more than three values", () =>{
+          expect(playground.movements.length).toBeLessThan(4);
+        });
+      });
+    }
+
+  });
+});
