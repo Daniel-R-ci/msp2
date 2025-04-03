474c010913ebe706afcda5dbcf79ab7078ff1fa2
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-04-03 13:08:56 +0200

    Add more js testing scenarios and document them in ReadMe

--- assets/scripts/playgrounds.js
+++ assets/scripts/playgrounds.js
@@ -17,14 +17,23 @@ const playgrounds = [
   {
     "name": "Test park 1", //Required value
     "area": "Hisingen",
-    "movements": ["ground", "feet", "water"], //Required array, must contain at least one item
-    "description": "",
+    "movements": ["ground", "feet", "air"], //Required array, must contain at least one item
+    "description": "A nice medium sized park with various equipment to keep play challenging and fun.",
     "geoX": "",
     "geoY": "",
-    "adress": "", //Required value
+    "adress": "Hisingen street", //Required value
     "image": "",
   },
-
+  {
+    "name": "Test park 2", //Required value
+    "area": "West", //Required value
+    "movements": ["feet", "air"], //Required array, must contain at least one item
+    "description": "A simple park but with good areas to run around (including elevation changes), swings and a challenging climbing wall suited for toddlers",
+    "geoX": "",
+    "geoY": "",
+    "adress": "Ruddalen entrance", //Required value
+    "image": "",
+  },
 ];
 
 module.exports = playgrounds;
--- assets/scripts/tests/playgrounds.test.js
+++ assets/scripts/tests/playgrounds.test.js
@@ -7,51 +7,65 @@ describe("Playgrounds validation", () => {
     test("Playgrounds array length is greater than 0 ", () => {
       expect(playgrounds.length).toBeGreaterThan(0);
     });
-    for (let playground of playgrounds) {
-      describe("Every object in playgrounds array should have eight keys, and the correct keys", () => {
-        test("Make sure playground eight keys", () => {
-          expect(Object.keys(playground).length).toEqual(8);
-        });
-        test("Current playground has name key", () => {
-          expect(playground.name).toBeDefined();
-        });
-        test("Current playground has name key", () => {
-          expect(playground.area).toBeDefined();
-        });
-        test("Current playground has movements key", () => {
-          expect(playground.movements).toBeDefined();
-        });
-        test("Current playground has description key", () => {
-          expect(playground.description).toBeDefined();
-        });
-        test("Current playground has geoX key", () => {
-          expect(playground.geoX).toBeDefined();
-        });
-        test("Current playground has geoY key", () => {
-          expect(playground.geoY).toBeDefined();
-        });
-        test("Current playground has geoY key", () => {
-          expect(playground.geoY).toBeDefined();
-        });
-        test("Current playground has adress key", () => {
-          expect(playground.adress).toBeDefined();
-        });
-      });
-      describe("Required fields should have values", () =>{
-        test("Name should have value", () =>{
-          expect(playground.name.length).toBeGreaterThan(0);
-        });
-        test("Area should have value", () =>{
-          expect(playground.area.length).toBeGreaterThan(0);
-        });
-        test("Movements should have values", () =>{
-          expect(playground.movements.length).toBeGreaterThan(0);
-        });
-        test("Movements should not have more than three values", () =>{
-          expect(playground.movements.length).toBeLessThan(4);
-        });
-      });
-    }
-
   });
+  for (let playground of playgrounds) {
+    describe("Every object in playgrounds array should have eight keys, and the correct keys", () => {
+      test("Make sure playground have eight keys", () => {
+        expect(Object.keys(playground).length).toEqual(8);
+      });
+      test("Current playground has name key", () => {
+        expect(playground.name).toBeDefined();
+      });
+      test("Current playground has name key", () => {
+        expect(playground.area).toBeDefined();
+      });
+      test("Current playground has movements key", () => {
+        expect(playground.movements).toBeDefined();
+      });
+      test("Current playground has description key", () => {
+        expect(playground.description).toBeDefined();
+      });
+      test("Current playground has geoX key", () => {
+        expect(playground.geoX).toBeDefined();
+      });
+      test("Current playground has geoY key", () => {
+        expect(playground.geoY).toBeDefined();
+      });
+      test("Current playground has geoY key", () => {
+        expect(playground.geoY).toBeDefined();
+      });
+      test("Current playground has adress key", () => {
+        expect(playground.adress).toBeDefined();
+      });
+    });
+    describe("Required fields should have values", () => {
+      test("Name should have value", () => {
+        expect(playground.name.length).toBeGreaterThan(0);
+      });
+      test("Area should have value", () => {
+        expect(playground.area.length).toBeGreaterThan(0);
+      });
+      test("Movements should have at least one value", () => {
+        expect(playground.movements.length).toBeGreaterThan(0);
+      });
+      test("Movements should not have more than three values", () => {
+        expect(playground.movements.length).toBeLessThan(4);
+      });
+      describe("Movements array should only contain accepted values", () =>{
+        var numMovements = 0;
+          for(let movement of playground.movements){
+            if(movement==="ground")
+              numMovements++;
+            if(movement==="feet")
+              numMovements++;
+            if(movement==="air")
+              numMovements++;
+          }
+          test("numMovements (number of matched movement types) shold match movements array length", () =>{
+            expect(numMovements).toEqual(playground.movements.length);
+          })
+      });
+    });
+  }
+
 });
